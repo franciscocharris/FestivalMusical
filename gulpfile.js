651a178365b283011cbd14a5a52e0ae2dcec57c0
franciscocharris <franciscocharrisc@gmail.com>
2021-06-22 17:17:37 -0500

    desde festivalMusical inicio, automatizando la compilacion scss a css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 // corchetes porque gulp tiene muchas funciones y dentro de esas llaves estarian todas las que se requieren
-const { src, dest } = require('gulp')
+// ejecutar multiples tareas, buscar un archivo, poner un destino , observar cambios y ejecutar tarea 
+const { series, src, dest, watch }  = require('gulp');
 // este no nesecita porque esta dependencia solo tiene una funcion
-const sass = require('gulp-sass')
+const sass = require('gulp-sass');
+// sass.compiler = require('');
 
-sass.compiler = require('dart-sass');
-
-function compilarSASS(){
-    return src("./src/scss/app.scss")
+function css(  ){
+    
+    return src('./src/scss/app.scss')
         .pipe( sass() )
-        .pipe( dest("./build/css") );
+        .pipe( dest('./build/css') );
+
 }
 
-// nombre de la tarea en gulp = nombre de la funcion o tarea 
-exports.compilarSASS = compilarSASS
+function minificarCss(  ){
+    
+    return src('./src/scss/app.scss')
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }) )
+        .pipe( dest('./build/css') );
+
+}
+
+function watchArchivos(){
+    // observar archivos y ejecutar tarea 
+    watch('./src/scss/app.scss', css);
+}
+
+exports.css = css
+exports.minificarCss = minificarCss
+exports.watchArchivos = watchArchivos
+
+// series sirve para ejecutar muchas tareas muchas funciones con una sola llamada
+// exports.tareas = series( css, js);
+// exports.default = series( css);
